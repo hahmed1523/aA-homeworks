126dfd5e619fafa2696ed6433fe4d8d7b15e5cd9
hahmed1523 <hahmed1523@gmail.com>
2021-05-12 12:36:09 -0400

    Added Stack class

---
+++ W1D3/dyi_adts.rb
@@ -0,0 +1,24 @@
+class Stack
+    def initialize
+        @ivar = []
+    end
+
+    #adds an element to the stack
+    def push(el)
+        @ivar << el 
+    end
+
+    #removes one element from the stack
+    def pop
+        @ivar.pop 
+    end
+
+    #returns, but doesnt remove, the top element in the stack
+    def peek
+        return @ivar[-1]
+    end
+end
+
+class Queue
+    
+end
